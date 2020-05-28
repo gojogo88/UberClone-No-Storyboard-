8c1d1fe15cbae1bb24bf8fcaa8b25238d4d0d5a7
Jonathan Go <gojonathan.dev@gmail.com>
2020-05-28 09:29:19 +0800

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Jonathan Go on 5/28/20.
+//  Copyright © 2020 SonnerStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
